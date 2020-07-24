9d27e1c9f0068e6bc25073eb02694a3a7a0704fb
Lucas Malta <lhqmalta@gmail.com>
2020-07-24 00:12:29 -0300

    Better logging

--- cmaker/src/main.ts
+++ cmaker/src/main.ts
@@ -1,14 +1,11 @@
 import { createSocket } from 'dgram'
 import { Buffer } from 'buffer'
-import { address } from 'ip'
 import { config } from 'dotenv'
+import { address } from 'ip'
 
 config()
 const client = createSocket('udp4')
 client.bind(4444)
-
-const localIP = address()
-const data = Buffer.from(localIP)
 
 const masterIP = process.env.MASTER_IP
 
@@ -17,21 +14,12 @@ client.on('message', (msg, info) => {
   console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port)
 })
 
-client.send(data, 5000, masterIP, (error) => {
-  if (error) {
-    client.close()
-  } else {
-    console.log('Data sent !!!')
-  }
+client.on('listening', () => {
+  const info = client.address()
+  const localIP = address()
+  const data = Buffer.from(`${localIP}:${info.port}`)
+  console.log('Sending data')
+  client.send(data, 5000, masterIP,
+    (error) => console.log('error:' + error)
+  )
 })
-
-// const data1 = Buffer.from('hello')
-// const data2 = Buffer.from('world')
-
-// client.send([data1, data2], 5000, masterIP, (error) => {
-//   if (error) {
-//     client.close()
-//   } else {
-//     console.log('Data sent')
-//   }
-// })
